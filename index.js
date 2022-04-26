ec7bcc1ec72c972e4e1d464f13746c593ffcd639
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-26 13:15:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('TheMystic-Bot\nWhatsApp Bot', {
+say('WhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`Bot creado por Bruno Sobrino`, {
+say(`by gatito`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
